867be942dc6faa921486ac1199bfd96163c81681
cky24ykc <cky24ykc@gmail.com>
2019-09-01 14:46:05 +0800

    import

---
+++ 射栗子/Assets/Scripts/IgaguriController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+}
